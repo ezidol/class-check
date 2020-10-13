f137cb4ee79899d504e97850aace1efdf9940cb8
mac <mac@macui-MacBookPro.local>
2020-10-13 10:22:56 +0900

    third

--- src/components/Class/Students/Student/Student.js
+++ src/components/Class/Students/Student/Student.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../../../hoc/Aux';
+import Aux from '../../../../hoc/Auxs';
 import WithClass from '../../../../hoc/withClass';
 import classes from './Student.css';
 
